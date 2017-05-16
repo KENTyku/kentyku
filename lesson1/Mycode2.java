acc92d5c357a6f5e3747059d9acb957e72b42d35
Tveritin Yuriy <kentyku@bk.ru>
2017-05-16 23:23:46 +0400

    Моя первая программа

---
+++ lesson1/Mycode2.java
@@ -0,0 +1,32 @@
+// моя первая программа на Java. Программа создает несколько переменных разных типов и инициализирует их значения. Для наглядности выводит значения переменных на экран
+
+public class Mycode2 {
+	
+		public static void main(String [] args) {
+			byte b=127;
+			short s=30000;
+			int i= 20;
+			long l=1000000;
+			float f=160.0f;
+			double d=15.75;
+			boolean bool=true;
+			char c='A';
+			
+			System.out.println	("b="+b);
+			System.out.println	("s="+s);
+			System.out.println	("i="+i);
+			System.out.println	("l="+l);
+			System.out.println	("f="+f);
+			System.out.println	("d="+d);
+			System.out.println	("bool="+bool);
+			System.out.println	("c="+c);
+		}
+		
+}
+// однострочный комментари21
+/* многострочный
+коммё
+*/
+/** 
+* документирующий комё
+*/
