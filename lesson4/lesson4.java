76f0d5c560597e513d1fb6141e14e369d1d8d7d5
Tveritin Yuriy <kentyku@bk.ru>
2017-05-17 09:01:15 +0400

    Урок 4 ДЗ 1 Крестики-Нолики консольный вариант

---
+++ lesson4/lesson4.java
@@ -0,0 +1,12 @@
+/*	Java Lesson 4
+*	avtor: Tveritin Yuri
+*	version: 15.05.2017
+*/
+import java.util.Random;
+import java.util.Scanner;
+public class lesson4 {
+	public static void main(String [] args) { 
+	System.out.println("Hello word!");
+	//
+	}
+}
