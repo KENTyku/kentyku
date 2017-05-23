0535b9314dfc138e6c1ff99831df30b595f48e6e
Tveritin Yuriy <kentyku@bk.ru>
2017-05-23 12:48:26 +0400

    Lesson5ДЗ. Добавил выбор сотрудников старше 40 лет

--- lesson5/lesson5.java
+++ lesson5/lesson5.java
@@ -8,9 +8,26 @@ class lesson5 {
 		  "+7(909)999-88-77",50000,33);//создание объекта типа Person
 			//и присвоение ссылки на него переменной p
 		p.printInfo();	//выполенение метода для созданного объекта p
-	}
+		
+		Person[] persArray=new Person[5];//создали массив для хранения ссылок
+		 //на 5 объектов типа Person
+		persArray[0]= new Person("Ivanov Ivan", "Engineer", 
+		 "ivivan@mailbox.com", "192312312", 10000, 30);
+		persArray[1]= new Person("Bivanov Bivan", "Engineer", 
+		 "bivivan@mailbox.com", "292312312", 20000, 41);
+		persArray[2]= new Person("Ivanov Ivan", "Engineer", 
+		 "ivivan@mailbox.com", "392312312", 30000, 28);
+		persArray[3]= new Person("Ivanov Ivan", "Engineer", 
+		 "ivivan@mailbox.com", "492312312", 40000, 42);
+		persArray[4]= new Person("Ivanov Ivan", "Engineer", 
+		 "ivivan@mailbox.com", "592312312", 50000, 36);
+		 //вывод информации о сотрудниках старше 40 лет
+		for (int i=0;i<5;i++){
+			if (persArray[i].age>40) persArray[i].printInfo();
+		}
+	}	
 }
-
+//Создание класса "Сотрудник":
 class Person {
 	//объявление переменных экземпляра класса
 	String fio;
@@ -36,6 +53,7 @@ class Person {
 		System.out.println(email);
 		System.out.println(fone);
 		System.out.println(money);
+		System.out.println(age);
 	}		
 	
 }
