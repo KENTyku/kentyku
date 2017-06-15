e37802ca309373b9eb6014c5add6116390c8c298
Tveritin Yuriy <kentyku@bk.ru>
2017-06-15 22:17:38 +0400

    HWJ2L3Collections

---
+++ J2L3/HWJ2L3/HWJ2L3Collections.java
@@ -0,0 +1,53 @@
+/*	HW Java2 Lesson 3
+*	avtor: Tveritin Yuri
+*	version: 15.06.2017
+*	Arrays Words 
+*/
+public class HWJ2L3Collections{
+	
+	public static void main(String[] args){
+		String[] arraywords=new String[4];
+		arraywords={"Cat", "Dog"};
+		
+	/*	new J2L2Errors().doErrors();
+	}
+	String st="1 k a 2\n2 3 2 2";//для проверки
+	//String st="1 3 1 2\n2 3 2 2\n5 6 7 1\n3 3 1 0";
+	String [][] arr= new String[4][4];
+	int sum=0;	
+		
+	void doErrors(){
+		System.out.println(st);
+		System.out.println(doArray(st));
+		
+	}
+	float doArray(String s){		
+		
+		for (int i=0; i<4;i++){
+			for (int j=0; j<4;j++){
+				try{
+					arr[i][j]=s.substring(0,1);//вырезает из строки подстроку с нач по конечный элемент
+					s=s.substring(1);//вырезает из строки подстроку с нач элемента и до конца
+					s=s.trim();//вырезает из строки подстроку без пробелов в начале и конце строки
+				}
+				catch(IndexOutOfBoundsException ex1){
+					System.out.println(ex1);
+					arr[i][j]="0";//присваиваем элементам массива (которым не передалось значение из строки) значение 0
+				}
+			}					
+		}
+		
+		for (int i=0; i<4;i++){
+			for (int j=0; j<4;j++){
+				try {
+					sum=sum+Integer.parseInt(arr[i][j]);//преобразование строки в целый тип			
+				} 
+				catch(NumberFormatException ex2) {
+					System.out.println(ex2);
+					arr[i][j]="0";
+				}
+			}			
+		}
+		return (sum/2);			*/
+	}
+}
